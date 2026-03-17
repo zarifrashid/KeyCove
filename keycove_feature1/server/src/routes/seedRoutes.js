efdcb4475f1f4815f7b6db9c0046d99786d864e6
Zarif Rashid <zarifrashid2004@gmail.com>
2026-03-18 05:37:51 +0600

    Clean duplicate project structure

--- keycove_feature1/server/src/routes/seedRoutes.js
+++
@@ -1,8 +0,0 @@
-import express from 'express'
-import { seedDhakaProperties } from '../controllers/seedController.js'
-
-const router = express.Router()
-
-router.post('/dhaka-properties', seedDhakaProperties)
-
-export default router
